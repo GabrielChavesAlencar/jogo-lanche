b330c878dbacddddb9f263e98db789c0e7538930
gabriel <c.a.gabriel2011@hotmail.com>
2023-06-15 14:27:29 -0300

    ajuste de posicao dos lanches na ui

---
+++ Assets/codigos/ajuste_pos_ingrediente.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ajuste_pos_ingrediente : MonoBehaviour
+{
+    public GameObject item_abaixo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ajustar();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ajustar() {
+        string temp = item_abaixo.GetComponent<Image>().sprite.name;
+        string nome = GetComponent<Image>().sprite.name;
+        //print("nome: "+ temp);
+        if (temp == "pao parte de baixo") {
+            if (nome == "hamburguer") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.08f, 0); }
+            else if (nome == "queijo") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.03f, 0); }
+            else if (nome == "alface") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.01f, 0); }
+            else if (nome == "tomate") { transform.position = item_abaixo.transform.position + new Vector3(-0.06f, 0.2f, 0); }
+            else if (nome == "picles") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.15f, 0); }
+            
+        }
+        else if (temp == "hamburguer") {
+            if (nome == "hamburguer") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.16f, 0); }
+            else if (nome == "queijo") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.13f, 0); }
+            else if (nome == "alface") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.07f, 0); }
+            else if (nome == "tomate") { transform.position = item_abaixo.transform.position + new Vector3(-0.06f, 0.3f, 0); }
+            else if (nome == "picles") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.22f, 0); }
+            else if (nome == "pao parte de cima") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.38f, 0); }
+        }
+        else if (temp == "queijo")
+        {
+            if (nome == "hamburguer") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.05f, 0); }
+            else if (nome == "queijo") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.13f, 0); }
+            else if (nome == "alface") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.01f, 0); }
+            else if (nome == "tomate") { transform.position = item_abaixo.transform.position + new Vector3(-0.06f, 0.2f, 0); }
+            else if (nome == "picles") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.1f, 0); }
+            else if (nome == "pao parte de cima") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.25f, 0); }
+        }
+        else if (temp == "alface")
+        {
+            if (nome == "hamburguer") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.1f, 0); }
+            else if (nome == "queijo") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.13f, 0); }
+            else if (nome == "alface") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.001f, 0); }
+            else if (nome == "tomate") { transform.position = item_abaixo.transform.position + new Vector3(-0.06f, 0.23f, 0); }
+            else if (nome == "picles") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.16f, 0); }
+            else if (nome == "pao parte de cima") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.27f, 0); }
+        }
+        else if (temp == "tomate")
+        {
+            print("nome: " + nome);
+            if (nome == "hamburguer") { transform.position = item_abaixo.transform.position + new Vector3(0.06f, 0.2f, 0); }
+            else if (nome == "queijo") { transform.position = item_abaixo.transform.position + new Vector3(0.06f, 0.13f, 0); }
+            else if (nome == "alface") { transform.position = item_abaixo.transform.position + new Vector3(0.06f, 0.001f, 0); }
+            else if (nome == "tomate") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.23f, 0); }
+            else if (nome == "picles") { transform.position = item_abaixo.transform.position + new Vector3(0.06f, 0.02f, 0); }
+            else if (nome == "pao parte de cima") { transform.position = item_abaixo.transform.position + new Vector3(0.06f, 0.15f, 0); }
+        }
+        else if (temp == "picles")
+        {
+            if (nome == "hamburguer") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.013f, 0); }
+            else if (nome == "queijo") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.13f, 0); }
+            else if (nome == "alface") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.001f, 0); }
+            else if (nome == "tomate") { transform.position = item_abaixo.transform.position + new Vector3(-0.06f, 0.16f, 0); }
+            else if (nome == "picles") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.1f, 0); }
+            else if (nome == "pao parte de cima") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.23f, 0); }
+        }
+    }
+    private void OnValidate()
+    {
+        ajustar();
+    }
+}
