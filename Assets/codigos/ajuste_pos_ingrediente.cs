804f89b5960a3d21f7bfa94ce924174fd62adbfd
gabriel <c.a.gabriel2011@hotmail.com>
2023-06-15 17:51:34 -0300

    adicionado sistema de pontuacao no jogo

--- Assets/codigos/ajuste_pos_ingrediente.cs
+++ Assets/codigos/ajuste_pos_ingrediente.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class ajuste_pos_ingrediente : MonoBehaviour
 {
     public GameObject item_abaixo;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +16,15 @@ public class ajuste_pos_ingrediente : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ajustar();
     }
+
+    //atualiza a posição dos ingredientes na UI
     public void ajustar() {
         string temp = item_abaixo.GetComponent<Image>().sprite.name;
         string nome = GetComponent<Image>().sprite.name;
-        //print("nome: "+ temp);
+       
+
         if (temp == "pao parte de baixo") {
             if (nome == "hamburguer") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.08f, 0); }
             else if (nome == "queijo") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.03f, 0); }
@@ -44,7 +48,7 @@ public class ajuste_pos_ingrediente : MonoBehaviour
             else if (nome == "alface") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.01f, 0); }
             else if (nome == "tomate") { transform.position = item_abaixo.transform.position + new Vector3(-0.06f, 0.2f, 0); }
             else if (nome == "picles") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.1f, 0); }
-            else if (nome == "pao parte de cima") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.25f, 0); }
+            else if (nome == "pao parte de cima") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.26f, 0); }
         }
         else if (temp == "alface")
         {
@@ -57,9 +61,8 @@ public class ajuste_pos_ingrediente : MonoBehaviour
         }
         else if (temp == "tomate")
         {
-            print("nome: " + nome);
             if (nome == "hamburguer") { transform.position = item_abaixo.transform.position + new Vector3(0.06f, 0.2f, 0); }
-            else if (nome == "queijo") { transform.position = item_abaixo.transform.position + new Vector3(0.06f, 0.13f, 0); }
+            else if (nome == "queijo") { transform.position = item_abaixo.transform.position + new Vector3(0.06f, -0.08f, 0); }
             else if (nome == "alface") { transform.position = item_abaixo.transform.position + new Vector3(0.06f, 0.001f, 0); }
             else if (nome == "tomate") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.23f, 0); }
             else if (nome == "picles") { transform.position = item_abaixo.transform.position + new Vector3(0.06f, 0.02f, 0); }
@@ -68,7 +71,7 @@ public class ajuste_pos_ingrediente : MonoBehaviour
         else if (temp == "picles")
         {
             if (nome == "hamburguer") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.013f, 0); }
-            else if (nome == "queijo") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.13f, 0); }
+            else if (nome == "queijo") { transform.position = item_abaixo.transform.position + new Vector3(0, -0.02f, 0); }
             else if (nome == "alface") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.001f, 0); }
             else if (nome == "tomate") { transform.position = item_abaixo.transform.position + new Vector3(-0.06f, 0.16f, 0); }
             else if (nome == "picles") { transform.position = item_abaixo.transform.position + new Vector3(0, 0.1f, 0); }
@@ -77,6 +80,7 @@ public class ajuste_pos_ingrediente : MonoBehaviour
     }
     private void OnValidate()
     {
-        ajustar();
+        //
+       /// ajustar();
     }
 }
--- Assets/codigos/jogo.cs
+++ Assets/codigos/jogo.cs
@@ -9,13 +9,23 @@ using TMPro;
 
 public class jogo : MonoBehaviour
 {
-    [SerializeField] SanduicheSO sanduicheSO;
+    [SerializeField] TMP_Text tempo_text;
+    [SerializeField] TMP_Text score_text;
+    public float tempo;
+
+    public int score;
+    private bool marcar_ponto;
+    private bool diminuir_ponto;
+    public static float tempo_espera;
+    //[SerializeField] SanduicheSO sanduicheSO;
 
     [SerializeField] SanduicheSO [] sanduiches;
 
     public Camera cam;
     public string [] nomes_ingrediente;
     public bool seguir_mouse;
+    public string [] ingredientes_colocados;
+
 
 
     public RectTransform item;
@@ -40,64 +50,71 @@ public class jogo : MonoBehaviour
 
     public Sprite[] sprites_ingrediente;
     public Image[] imgs_ingrediente;
+    public int numero_lanche;
+
+    
 
 
-
-    // Start is called before the first frame update
+    
     void Start()
     {
-
-        updateItem();
+        tempo_espera = 0;
+        numero_lanche = Random.Range(0, sanduiches.Length);
+        updateItem(numero_lanche);
 
     }
     private void OnValidate()
     {
-        updateItem();
+       // updateItem(8);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        updateItem(numero_lanche);
         if (seguir_mouse) {
             MoveObject();
 
         }
-        if (layer_item == 3) {
-            Instantiate(pao, new Vector3(-0.15f, 1.3f, 0), Quaternion.identity);
-            layer_item = 0;
-        }
+        
         if (Input.GetKeyDown(KeyCode.A)) { SceneManager.LoadScene("SampleScene"); }
+       
+        proximo_lanche();
 
     }
-    public void updateItem() {
-        nome_lanche.SetText(sanduicheSO.nome);
-        lanche_descricao.SetText("Primeiramente adicione "+sanduicheSO.ingredientes[0]+ " depois coloque " + sanduicheSO.ingredientes[1] +" e por fim acrecente " + sanduicheSO.ingredientes[2]);
+
+    //atualiza o icone e as informações do lanche na UI
+    public void updateItem(int ind) {
+        nome_lanche.SetText(sanduiches[ind].nome);
+        lanche_descricao.SetText("Primeiramente adicione "+ sanduiches[ind].ingredientes[0]+ " depois coloque " + 
+            sanduiches[ind].ingredientes[1] +" e por fim acrecente " + sanduiches[ind].ingredientes[2]);
 
         for (int i = 0; i <= 2; i++) {
-            nomes_ingrediente[i] = sanduicheSO.ingredientes[i] + "";
+            nomes_ingrediente[i] = sanduiches[ind].ingredientes[i] + "";
             if (nomes_ingrediente[i] == "alface") { imgs_ingrediente[i].sprite = sprites_ingrediente[0]; }
             else if (nomes_ingrediente[i] == "hamburguer") { imgs_ingrediente[i].sprite = sprites_ingrediente[1]; }
             else if (nomes_ingrediente[i] == "picles") { imgs_ingrediente[i].sprite = sprites_ingrediente[2]; }
             else if (nomes_ingrediente[i] == "queijo") { imgs_ingrediente[i].sprite = sprites_ingrediente[3]; }
             else if (nomes_ingrediente[i] == "tomate") { imgs_ingrediente[i].sprite = sprites_ingrediente[4]; }
         }
-     //   if (sanduicheSO.ingredientes[0] + "" == "alface") { print("deu certo alface!"); }
+    
 
     }
     public void selecionar_ingrediente(int num) {
-        if (!seguir_mouse)
+        if (!seguir_mouse&&layer_item<3)
         {
 
             seguir_mouse = true;
             indice_ingrediente = num;
-            if (num == 0) { item = alface_UI; }
-            else if (num == 1) { item = hamburger_UI; }
-            else if (num == 2) { item = picles_UI; }
-            else if (num == 3) { item = queijo_UI; }
-            else if (num == 4) { item = tomate_UI; }
+            if (num == 0) { item = alface_UI; ingredientes_colocados[layer_item] = "alface"; }
+            else if (num == 1) { item = hamburger_UI; ingredientes_colocados[layer_item] = "hamburguer"; }
+            else if (num == 2) { item = picles_UI; ingredientes_colocados[layer_item] = "picles"; }
+            else if (num == 3) { item = queijo_UI; ingredientes_colocados[layer_item] = "queijo"; }
+            else if (num == 4) { item = tomate_UI; ingredientes_colocados[layer_item] = "tomate"; }
             pos_inicial_item = item.transform.position;
         }
     }
+
+    //faz o ingrediente seguir o mouse
     public void MoveObject() {
         Vector3 pos = Input.mousePosition;
         pos.z = 100;
@@ -112,7 +129,50 @@ public class jogo : MonoBehaviour
             temp.GetComponent<SpriteRenderer>().sortingOrder = layer_item;
             seguir_mouse = false;
             item.transform.position = pos_inicial_item;
+            /*
+            if (layer_item == 3)
+            {
+               
+                layer_item = 0;
+            }
+            */
+            if (layer_item >= 3){atulizarpontuacao();}
         }
 
+    }
+
+    public void atulizarpontuacao() {
+        int cont = 0;
+        for (int i = 0; i <= 2; i++)
+        {
+            if (nomes_ingrediente[i] == ingredientes_colocados[i]) { cont++; }
+        }
+        if (cont == 3) { marcar_ponto = true; }
+        else { diminuir_ponto = true; }
+        Instantiate(pao, new Vector3(-0.15f, 1.3f, 0), Quaternion.identity);
+       
+    }
+
+    public void proximo_lanche() { 
+        if (marcar_ponto|| diminuir_ponto) { tempo_espera += Time.deltaTime; }
+        if (tempo_espera > 1 && tempo_espera < 1.1f) { 
+            if (marcar_ponto) { score++; }
+            if (diminuir_ponto) { score--; }
+            score_text.SetText("SCORE: " + score.ToString("00"));
+            tempo_espera = 1.1f;
+        }
+        if (tempo_espera > 4) {
+            marcar_ponto = false;
+            diminuir_ponto = false;
+            tempo_espera = 0;
+            layer_item = 0;
+            for (int i = 0; i <= 2; i++)
+            {
+                ingredientes_colocados[i] = "";
+            }
+            numero_lanche = Random.Range(0, sanduiches.Length);
+            updateItem(numero_lanche);
+            
+        }
     }
 }
